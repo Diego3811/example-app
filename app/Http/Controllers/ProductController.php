bb4a6a991defe394d750e5e686304e07abb90a76
Diego3811 <diegohuci12@gmail.com>
2024-08-16 17:22:06 -0600

    seccond commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view ('admin.product.home');
+
+        $products = Product::orderBy('id', 'desc')->get();
+        $total = Product::count();
+        return view('admin.product.home', compact(['products']));
+    }
+
+    public function create()
+    {
+        return view('admin.product.create');
+    }
+    public function save(Request $request)
+    {
+        $validation = $request->validate([
+            'title' => 'Required',
+            'category' => 'Required',
+            'price' => 'Required'
+
+        ]);
+        $data = Product::create($validation);
+        if ($data) {
+            session()->flash('success', 'Product Add Successfully');
+            return redirect(route('admin/products'));
+        } else {
+            session()->flash('error', 'Some problem occured');
+            return redirect(route('admin.products/create'));
+        }
+    }
+
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('admin.product.update', compact('product'));
+    }
+
+
+
+    public function delete($id)
+    {
+        $product = Product::findOrFail($id)->delete();
+        if ($product) {
+            session()->flash('success', 'Product Deleted Successfully');
+            return redirect(route('admin/products'));
+        } else {
+        }
+        session()->flash('error', 'Product Not Delete successfully');
+        return redirect(route('admin/products'));
+        $products = Product::findOrFail($id)->delete();
+    }
+
+    public function Update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $title = $request->title;
+        $category = $request->category;
+        $price = $request->price;
+
+        $product->title = $title;
+        $product->category = $category;
+        $product->price = $price;
+        $data = $product->save();
+
+        if ($data) {
+            session()->flash('success', 'Product Update Successfully');
+            return redirect(route('admin/products'));
+        } else {
+            session()->flash('error', 'Some problem occured');
+            return redirect(route('admin/products/update'));
+        }
     }
 }
--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -1,12 +1,14 @@
 <?php
+ 
+ namespace App\Http\Middleware;
 
-namespace App\Http\Middleware;
 
+ 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
-
+use Symfony\Component\HttpFoundation\Response;
+ 
 class Admin
 {
     /**
@@ -16,10 +18,10 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-       
-        if (Auth::user()->usertype != 'admin'){
-             return redirect('dashboard');
+        if (Auth::user()->usertype != 'admin') {
+            return redirect('dashboard');
         }
+ 
         return $next($request);
     }
 }
