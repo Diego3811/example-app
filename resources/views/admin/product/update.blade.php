73dedc8fb0c21504180bbace359fccad11236cf9
Diego3811 <diegohuci12@gmail.com>
2024-08-17 12:12:22 -0600

    Co-authored-by: Ashrah <Ashrahx@users.noreply.github.com>

--- resources/views/admin/product/update.blade.php
+++ resources/views/admin/product/update.blade.php
@@ -1,52 +1,46 @@
 <x-app-layout>
-    slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight"> {{ _('Edit Product') }}
-    </h2>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Product') }}
+        </h2>
     </x-slot>
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm: px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm: rounded-lg">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1 class="mb-0">Edit Product</h1>
-                    <hr />
-                    Form
-                </div>
+                    <h1 class="form-title">Edit Product</h1>
+                    <hr class="divider" />
 
-                <form action="{{ route('admin/products/update', $product->id) }}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    @error('title')
-                    <span class="text-danger">{{$message}}</span>
-                    @enderror
-                    <div class="row">
-                        <div class="col mb-3">
+                    <form action="{{ route('admin/products/update', $product->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        @error('title')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+
+                        <div class="form-group">
                             <label class="form-label">Product Name</label>
-                            <input type="text" name="title" class="form-control" placeholder="Title" value="{{$product->title}}">
+                            <input type="text" name="title" class="form-control" placeholder="Title" value="{{ $product->title }}">
                         </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col mb-3">
+                        <div class="form-group">
                             <label class="form-label">Category</label>
-                            <input type="text" name="category" class="form-control" placeholder="category" value="{{$product->category}}">
+                            <input type="text" name="category" class="form-control" placeholder="Category" value="{{ $product->category }}">
                         </div>
-                    </div>
+
+                        <div class="form-group">
+                            <label class="form-label">Price</label>
+                            <input type="text" name="price" class="form-control" placeholder="Product Price" value="{{ $product->price }}">
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn-warning">Update</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col mb-3">
-                <label class="form-label">Product Name</label>
-                <input type="text" name="price" class="form-control" placeholder="product-price" value="{{$product->price}}">
-            </div>
-        </div>
-
-
-        <div class="row">
-            <div class="d-grid">
-                <button class="btn btn-warning">Update</button>
-            </div>
-        </div>
-
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 
         <!-- Scripts -->
         
@@ -19,17 +20,10 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
             <!-- Page Content -->
             <main>
+                <x-header/>
+                <div class="py-12"></div>
                 {{ $slot }}
             </main>
         </div>
